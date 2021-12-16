1075071f25490a7700e4c5315c4eb9610a7d53c2
tanouch <ugo.tanielian@outlook.fr>
2021-12-16 19:31:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-#python3.6 -m venv ./venv 
-#source venv/bin/activate
-#pip install --upgrade pip
-#pip install -e .
-alias activate=". ./venv/bin/activate" 
+python3.6 -m venv ./venv 
+source venv/bin/activate
+pip install --upgrade pip
+pip install -e .
